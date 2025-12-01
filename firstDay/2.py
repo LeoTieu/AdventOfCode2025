78af47b05543e1fc26624ae0e44e4603b7e0a948
Mayo Layo <l.t05@outlook.com>
2025-12-02 00:11:47 +0100

    Second more optimized solution

--- firstDay/2.py
+++ firstDay/2.py
@@ -1,7 +1,7 @@
 def first_solution(puzzle_input: list) -> int:
     """
     First brute force solution
-    O(N*M) where N is the amount of inputs and M is the average turns 
+    O(N*M) where N is the amount of rotations and M is the average turns 
     """
     dial = 50
     counter = 0
@@ -25,7 +25,42 @@ def first_solution(puzzle_input: list) -> int:
     return counter
 
 
+def second_solution(puzzle_input: list) -> int:
+    """
+    Second solution
+    O(N) where N is the amount of rotations
+    """
+    dial = 50
+    counter = 0
+    for rotation in puzzle_input:
+        direction = rotation[0]
+        amount = (rotation[1::])
+
+        # highest number is 998
+        if len(amount) == 3:
+            counter += int(amount[0])
+            amount = int(amount[1::])
+        else:
+            amount = int(amount)
+            
+        if direction == "R":
+            dial += amount
+            if dial >= 100:
+                counter += 1
+                dial = dial % 100
+        else:
+            if dial == 0:
+                dial = (dial - amount) % 100
+            else:
+                dial -= amount
+                if dial <= 0:
+                    counter += 1
+                    dial %= 100
+    return counter
+
+
 if __name__ ==  '__main__':
     with open("PuzzleInput.txt", "r", encoding="utf-8") as file:
         puzzle_input = file.read().split()
     print(first_solution(puzzle_input))
+    print(second_solution(puzzle_input))
